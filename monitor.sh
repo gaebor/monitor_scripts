cfd98319e61453217a1b2ab66239e841938a81fe
Borbély Gábor <gaebor@gmail.com>
2017-06-04 23:08:12 +0200

    switch infos on/of

--- monitor.sh
+++ monitor.sh
@@ -4,6 +4,17 @@ mem_total=`free -mb | tail -n+2 | head -n1 | collapse | cut -f2 -d" "`
 
 num_cores=`grep -c ^processor /proc/cpuinfo`
 
+high_usage=False
+show_cores=False
+
+if [[ " ${*} " == *" -h "* ]]; then
+    high_usage=True
+fi
+
+if [[ " ${*} " == *" -c "* ]]; then
+    show_cores=True
+fi
+
 ps -Ao pcpu,rsz,fname,uname | tail -n+2 | sort -rgk1 | collapse | \
 python -c "
 from __future__ import print_function
@@ -21,6 +32,10 @@ for line in sys.stdin:
         high_mem_str = line
     sum_proc += line[0]
     sum_mem += line[1]
-print('%'+ ('%-5d' % sum_proc), '%-5s' % '/$num_cores', '%-19s' % ('(' + ' '.join(high_proc_str[2:]) + ')'),
-      int(1024*sum_mem), '/', $mem_total, '%-19s' % ('(' + ' '.join(high_mem_str[2:]) + ')'))
+
+high_proc_str = '%-19s' % ('(' + ' '.join(high_proc_str[2:]) + ')') if $high_usage else ''
+high_mem_str = '%-19s' % ('(' + ' '.join(high_mem_str[2:]) + ')') if $high_usage else ''
+cores_str = ('%-5s' % '/$num_cores') if $show_cores else ''
+print('%'+ ('%-5d' % sum_proc), cores_str, high_proc_str,
+      int(1024*sum_mem), '/', $mem_total, high_mem_str)
 "
