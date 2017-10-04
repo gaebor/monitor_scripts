44bda2d88da4e6ffcc312bfeda3be3f512b1dd29
gaebor <gaebor@gmail.com>
2017-10-04 23:09:04 +0200

    trimming partition backup

---
+++ backup.sh
@@ -0,0 +1,92 @@
+DEVICE=/dev/sdc
+
+if [[ $1 ]]
+then
+    DEVICE=$1
+fi
+
+echo "[ backing up $DEVICE ]"
+
+FOLDER=/mnt/Fury/BackUp
+
+OUTPUTNAME=$FOLDER/`basename $DEVICE`_`date +"%Y_%m_%d"`
+
+PARTED_INFO="`sudo parted $DEVICE unit B p | sed "s/^\s\+//" | grep -v "^\s*$" | tail -n1 | sed "s/\s\+/\t/g"`"
+
+echo "$PARTED_INFO"
+
+# last partition
+PARTITION=`cut -f1 <<< "$PARTED_INFO"`
+if [[ -z $PARTITION ]]
+then
+    exit 1
+fi
+
+FULLPARTITION=$DEVICE$PARTITION
+
+DISK_SIZE=`lsblk --output=NAME,SIZE -b -p | grep "^$DEVICE\s" | cut -d" " -f2-`
+if [[ -z $DISK_SIZE ]]
+then
+    exit 1
+fi
+
+echo "[ make full backup ]"
+sudo dd status=progress if=$DEVICE bs=1M of=$OUTPUTNAME.tmp
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+echo "[ checking partition ]"
+sudo e2fsck -f $FULLPARTITION
+if [[ $? -ne 0 ]]
+then 
+    exit 1
+fi
+
+echo "[ shrink the last partition"
+sudo resize2fs $FULLPARTITION -M 2>&1 | tee $OUTPUTNAME.log
+if [[ $? -ne 0 ]]
+then 
+    exit 1
+fi
+
+# find out how much did it manage to shrink
+# every variable is measured in Bytes
+SHRINKED=`grep "(4k)" $OUTPUTNAME.log | tail -n1 | python -c "import sys; l=sys.stdin.readline().strip().split(); pos=l.index('(4k)'); print(int(l[pos-1])*4096)"`
+if [[ -z $SHRINKED ]]
+then
+    exit 1
+fi
+
+START=`cut -f2 <<<"$PARTED_INFO" | cut -f1 -d"B"`
+
+NEW_END=$((START + 1 + (105*SHRINKED)/100))
+echo "[ resize $FULLPARTITION to $NEW_END bytes ]"
+sudo parted $DEVICE unit B resizepart $PARTITION $NEW_END
+if [[ $? -ne 0 ]]
+then
+exit 1
+fi
+
+sudo partprobe
+
+echo "[ make trimmed backup ]"
+sudo dd status=progress if=$DEVICE bs=1M of=$OUTPUTNAME.img \
+    count=$(((NEW_END + 1048575) / 1048576))
+if [[ $? -ne 0 ]]
+then
+    exit 1
+fi
+
+chmod 777 $OUTPUTNAME.img
+rm $OUTPUTNAME.tmp
+
+echo "[ extend to the full disk ]"
+DISK_SIZE=$((DISK_SIZE - 1))
+until sudo parted $DEVICE unit B resizepart $PARTITION $DISK_SIZE
+do
+    DISK_SIZE=$((DISK_SIZE - 1))
+done
+
+sudo parted $DEVICE unit B p | sed "s/^\s\+//" | grep -v "^\s*$" | tail -n1 | sed "s/\s\+/\t/g"
