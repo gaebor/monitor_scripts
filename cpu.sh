a84cbd045fd71d05f4e3a5bdc1791db6f9359b2a
Gabor Borbely <gaebor@gmail.com>
2021-02-20 20:27:39 +0100

    Hz and nicer bars

--- cpu.sh
+++ cpu.sh
@@ -1,15 +1,31 @@
-function provide_cpu_data() {
-    grep '^cpu' /proc/stat | tail -n+2 | python3 -c 'import sys; print(" ".join(map(str, (sum(map(int, line.strip().split()[1:4])) for line in sys.stdin))))'
+#!/bin/bash
+
+function print_bar() {
+    width="$1"
+    length="$2"
+    text=`echo -n "$3"; bash -c "printf ' %.0s' {1..$width}"`
+    text="${text::$width}"
+    echo -n -e "\e[7m"
+    echo -n "${text::$length}"
+    echo -n -e "\e[27m"
+    echo -n "${text:$length}"
 }
 
-CPU_stats=(`provide_cpu_data`)
-n_cpus=${#CPU_stats[*]}
+function provide_cpu_data() {
+    grep '^cpu' /proc/stat | tail -n+2 | \
+    while read line
+    do
+        echo -n $((`cut -f2,3,4 -d' ' <<<"$line" | sed 's/ /+/g'`))
+        echo -n ' '
+    done
+}
+
+CPU_STAT=(`provide_cpu_data`)
+n_cpus=`getconf _NPROCESSORS_ONLN`
 TIME=`date +%s`
-ticks=100
+ticks=`getconf CLK_TCK`
 
 time_interval=2
-# filler=`python3 -c "print('\u25AE', end='')"`
-filler='|'
 
 while true
 do
@@ -17,14 +33,14 @@ do
     COLUMNS=`tput cols`
     WIDTH=$(($COLUMNS/$n_cpus))
     NEW_STAT=(`provide_cpu_data`)
+    FREQS=(`grep MHz /proc/cpuinfo | cut -f2 -d: | tr '\n' ' '`)
     NEW_TIME=`date +%s`
     for i in `seq 0 $((n_cpus-1))`
     do
-        usage=$(($WIDTH*(${NEW_STAT[$i]}-${CPU_stats[$i]})/($NEW_TIME-$TIME)/$ticks))
-        for i in `seq 1 $usage`; do echo -n "$filler"; done
-        for i in `seq $(($usage+1)) $WIDTH`; do echo -n ' '; done
+        usage=$(($WIDTH * (${NEW_STAT[$i]}-${CPU_STAT[$i]}) / ($NEW_TIME-$TIME) / $ticks))
+        print_bar $WIDTH $usage ${FREQS[$i]}
     done
     echo
-    CPU_stats=(${NEW_STAT[*]})
+    CPU_STAT=(${NEW_STAT[*]})
     TIME=$NEW_TIME
 done
